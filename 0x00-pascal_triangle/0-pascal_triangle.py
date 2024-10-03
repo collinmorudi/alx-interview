f95f3972b7d00a8eee836c9e97fc5f09e3fd4055
Collin Morudi <collinmorudi@gmail.com>
2024-10-03 13:51:07 +0200

    Fix: pycode style errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
 
 def pascal_triangle(n):
+    """
+    print the pascal triangle
+    """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # The first row is always [1]
-    
+
     for i in range(1, n):
         row = [1]  # Start each row with 1
-        # Fill in the middle values as sums of adjacent elements from the previous row
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)  # End each row with 1
         triangle.append(row)
-    
+
     return triangle
